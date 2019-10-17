ec8890d4a994f35bfeea7ccc02bd8eb2b256addc
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2019-10-17 13:42:06 +0900

    WARNログが大量に出る問題に対処.

--- src/main/java/com/nablarch/example/app/web/action/IndustryAction.java
+++ src/main/java/com/nablarch/example/app/web/action/IndustryAction.java
@@ -1,17 +1,16 @@
 package com.nablarch.example.app.web.action;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.nablarch.example.app.entity.Industry;
+import com.nablarch.example.app.web.dto.IndustryDto;
+import nablarch.common.dao.UniversalDao;
+import nablarch.core.beans.BeanUtil;
+import nablarch.core.beans.CopyOptions;
+import nablarch.fw.dicontainer.web.RequestScoped;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import nablarch.common.dao.UniversalDao;
-import nablarch.core.beans.BeanUtil;
-import nablarch.fw.dicontainer.web.RequestScoped;
-
-import com.nablarch.example.app.entity.Industry;
-import com.nablarch.example.app.web.dto.IndustryDto;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 業種検索API
@@ -29,10 +28,10 @@ public class IndustryAction {
     @Produces(MediaType.APPLICATION_JSON)
     public List<IndustryDto> find() {
         List<Industry> industries = UniversalDao.findAll(Industry.class);
-
+        CopyOptions options = CopyOptions.options().excludes("clientList").build();
         return industries
                 .stream()
-                .map(industry -> BeanUtil.createAndCopy(IndustryDto.class, industry))
+                .map(industry -> BeanUtil.createAndCopy(IndustryDto.class, industry, options))
                 .collect(Collectors.toList());
     }
 }
