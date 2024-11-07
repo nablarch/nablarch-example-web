ab455d0d171c731054bc9ccbdd1c3a9e3b8fbcb2
kankichi <ichikawa.kankichi@tis.co.jp>
2024-11-07 18:17:46 +0900

    キーとして保存するトークンにprefixの追加とコードの整形

--- src/main/java/com/nablarch/example/app/web/token/RedisTokenManager.java
+++ src/main/java/com/nablarch/example/app/web/token/RedisTokenManager.java
@@ -13,38 +13,57 @@ import java.util.concurrent.TimeUnit;
  */
 public class RedisTokenManager implements TokenManager {
 
-    /** 接続先のRedisの構成ごとに用意された専用のクライアントクラス。
+    /**
+     * 接続先のRedisの構成ごとに用意された専用のクライアントクラス。
      * <p>
      * どのクライアントクラスを使用するかは、環境設定値 nablarch.lettuce.clientType で設定されており、
      * デフォルト値はredisstore-lettuce.config でLettuceSimpleRedisClientが呼ばれるようになっている。
      * 詳しくは以下の解説書を参考にすること。
+     *
      * @see <a href="https://nablarch.github.io/docs/6u2/doc/application_framework/adaptors/lettuce_adaptor/redisstore_lettuce_adaptor.html#redisstore-redis-client-config-client-classes">Redisストア(Lettuce)アダプタ - 構成ごとに用意されたクライアントクラス</a>
      */
     private LettuceRedisClient redisClient;
 
-    /** 有効期間(ミリ秒) */
+    /**
+     * 有効期間(ミリ秒)
+     */
     private Long expiresMilliSeconds;
+
+    /**
+     * 二重サブミット用のトークンを Redis に格納するときに使用するキーを作成する。
+     *
+     * @param redisToken トークン
+     * @return Redis への格納に使用するキー
+     */
+    private static String toRedisStoreKey(String redisToken) {
+        return "nablarch.double.submission." + redisToken;
+    }
 
     @Override
     public void saveToken(String serverToken, NablarchHttpServletRequestWrapper request) {
-        // トークンそのものをキーとして保存する。
+        String serverRedisStoreKey = toRedisStoreKey(serverToken);
+
+        // トークンそのものを紐づけてキーとして保存する。
         // トークンを保持することが目的なので、値はダミーを設定している。
         // ユーザーごとにトークンを管理したい場合はユーザーに紐づく情報をキーにするよう検討してください。
-        redisClient.set(serverToken,"dummy".getBytes(StandardCharsets.UTF_8));
+        redisClient.set(serverRedisStoreKey, "dummy".getBytes(StandardCharsets.UTF_8));
 
         // 有効期限を設定する。
-        // 有効期限はコンポーネント tokenManager のプロパティ expires で設定された値を使用する。
-        redisClient.pexpire(serverToken, expiresMilliSeconds);
+        // 有効期限はコンポーネント tokenManager のプロパティ expiresSeconds で設定された値を使用する。
+        redisClient.pexpire(serverRedisStoreKey, expiresMilliSeconds);
     }
 
     @Override
     public boolean isValidToken(String clientToken, ServletExecutionContext context) {
+        String clientRedisStoreKey = toRedisStoreKey(clientToken);
+
+        // クライアントトークンを紐づけたキーで値を取得できれば、トークンが有効であると判断できる。
         // サーバートークンが有効期限切れなどで存在しない場合は無効とする。
-        byte[] serverTokenBytes = redisClient.get(clientToken);
-        if(serverTokenBytes == null){
+        byte[] serverTokenBytes = redisClient.get(clientRedisStoreKey);
+        if (serverTokenBytes == null) {
             return false;
         }
-        redisClient.del(clientToken);
+        redisClient.del(clientRedisStoreKey);
         return true;
     }
 
